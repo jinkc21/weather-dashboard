ad248a7fdf092b3c583e2ceeda597274ceb36c9f
Jinkc21 <jin.k.choe@gmail.com>
2024-03-13 02:18:03 -0400

    applied day 1 forecast code to remaining days

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,29 @@ const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&l
   dayOneEl.children[3].innerHTML = "Humidity: " + data.list[0].main.humidity
   dayOneEl.children[4].innerHTML = "Wind Speed:" + data.list[0].wind.speed
 
+  dayTwoEl.children[0].innerHTML = data.list[8].dt_txt
+  dayTwoEl.children[1].src = "https://openweathermap.org/img/wn/"+data.list[8].weather[0].icon+"@2x.png"
+  dayTwoEl.children[2].innerHTML = "Temperature: " + data.list[8].main.temp
+  dayTwoEl.children[3].innerHTML = "Humidity: " + data.list[8].main.humidity
+  dayTwoEl.children[4].innerHTML = "Wind Speed:" + data.list[8].wind.speed
+
+  dayThreeEl.children[0].innerHTML = data.list[16].dt_txt
+  dayThreeEl.children[1].src = "https://openweathermap.org/img/wn/"+data.list[16].weather[0].icon+"@2x.png"
+  dayThreeEl.children[2].innerHTML = "Temperature: " + data.list[16].main.temp
+  dayThreeEl.children[3].innerHTML = "Humidity: " + data.list[16].main.humidity
+  dayThreeEl.children[4].innerHTML = "Wind Speed:" + data.list[16].wind.speed
+
+  dayFourEl.children[0].innerHTML = data.list[24].dt_txt
+  dayFourEl.children[1].src = "https://openweathermap.org/img/wn/"+data.list[24].weather[0].icon+"@2x.png"
+  dayFourEl.children[2].innerHTML = "Temperature: " + data.list[24].main.temp
+  dayFourEl.children[3].innerHTML = "Humidity: " + data.list[24].main.humidity
+  dayFourEl.children[4].innerHTML = "Wind Speed:" + data.list[24].wind.speed
+
+  dayFiveEl.children[0].innerHTML = data.list[32].dt_txt
+  dayFiveEl.children[1].src = "https://openweathermap.org/img/wn/"+data.list[32].weather[0].icon+"@2x.png"
+  dayFiveEl.children[2].innerHTML = "Temperature: " + data.list[32].main.temp
+  dayFiveEl.children[3].innerHTML = "Humidity: " + data.list[32].main.humidity
+  dayFiveEl.children[4].innerHTML = "Wind Speed:" + data.list[32].wind.speed
 }
  })
 }
