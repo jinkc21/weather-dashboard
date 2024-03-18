e10d4c765398b27cd41064b31a825960c2f91c09
Jinkc21 <jin.k.choe@gmail.com>
2024-03-18 15:16:32 -0400

    removed forecast button and combined forecast  and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const getWeatherBtn = document.getElementById('get-weather-button')
-const getForecastBtn = document.getElementById('get-forecast-button')
+// const getForecastBtn = document.getElementById('get-forecast-button')
 const weatherOutput = document.getElementById('weather-output')
 const cityInput = document.getElementById('city-input')
 const APIKey = "fa6f4ef3799ea75f4bd83e1146ea02a9"
 const cityNameEl = document.getElementById('city-name')
+const forecastEl = document.getElementById('forecast')
+const dayZeroEl = document.getElementById('day-0')
 const dayOneEl = document.getElementById('day-1')
 const dayTwoEl = document.getElementById('day-2')
 const dayThreeEl = document.getElementById('day-3')
@@ -32,66 +34,67 @@ function getWeather(event) {
       localStorage.setItem("coord", JSON.stringify(data.coord))
 
       cityNameEl.innerHTML = data.name
-      $('#day-0-date').text(today.format('YYYY-MM-D'));
-      dayOneEl.children[1].src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
-      dayOneEl.children[2].innerHTML = "Temperature: " + data.main.temp
-      dayOneEl.children[3].innerHTML = "Humidity: " + data.main.humidity
-      dayOneEl.children[4].innerHTML = "Wind Speed:" + data.wind.speed
-    
+      $('#day-0-date').text(today.format('YYYY-MM-D') + "   Today's Weather");
+      dayZeroEl.children[1].src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+      dayZeroEl.children[2].innerHTML = "Temperature: " + data.main.temp
+      dayZeroEl.children[3].innerHTML = "Humidity: " + data.main.humidity
+      dayZeroEl.children[4].innerHTML = "Wind Speed:" + data.wind.speed
+
       let cityList = document.createElement('button')
       cityList.textContent = recentSearch
       recentCities.appendChild(cityList)
 
-      
+
     })
-    recentCities.addEventListener("click", (event) => {
-      if(event.target.tagName === 'BUTTON') {
-        console.log(event.target.innerText)
-      }
-    })
+  recentCities.addEventListener("click", (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      console.log(event.target.innerText)
+    }
+  })
+  getForecast()
 }
 
 getWeatherBtn.addEventListener('click', getWeather);
-getForecastBtn.addEventListener('click', getForecast);
+// getForecastBtn.addEventListener('click', getForecast);
 
-      function getForecast(event) {
-        event.preventDefault()
-        const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=${APIKey}`
-        fetch(forecastUrl)
-             .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            console.log("hello")
-            cityNameEl.innerHTML = data.city.name
-            dayOneEl.children[0].innerHTML = data.list[0].dt_txt
-            dayOneEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
-            dayOneEl.children[2].innerHTML = "Temperature: " + data.list[0].main.temp
-            dayOneEl.children[3].innerHTML = "Humidity: " + data.list[0].main.humidity
-            dayOneEl.children[4].innerHTML = "Wind Speed:" + data.list[0].wind.speed
+function getForecast() {
+  // event.preventDefault()
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=${APIKey}`
+  fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("hello")
+      forecastEl.innerHTML = "5 Day Forecast"
+      dayOneEl.children[0].innerHTML = data.list[0].dt_txt
+      dayOneEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png"
+      dayOneEl.children[2].innerHTML = "Temperature: " + data.list[0].main.temp
+      dayOneEl.children[3].innerHTML = "Humidity: " + data.list[0].main.humidity
+      dayOneEl.children[4].innerHTML = "Wind Speed:" + data.list[0].wind.speed
 
-            dayTwoEl.children[0].innerHTML = data.list[8].dt_txt
-            dayTwoEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[8].weather[0].icon + "@2x.png"
-            dayTwoEl.children[2].innerHTML = "Temperature: " + data.list[8].main.temp
-            dayTwoEl.children[3].innerHTML = "Humidity: " + data.list[8].main.humidity
-            dayTwoEl.children[4].innerHTML = "Wind Speed:" + data.list[8].wind.speed
+      dayTwoEl.children[0].innerHTML = data.list[8].dt_txt
+      dayTwoEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[8].weather[0].icon + "@2x.png"
+      dayTwoEl.children[2].innerHTML = "Temperature: " + data.list[8].main.temp
+      dayTwoEl.children[3].innerHTML = "Humidity: " + data.list[8].main.humidity
+      dayTwoEl.children[4].innerHTML = "Wind Speed:" + data.list[8].wind.speed
 
-            dayThreeEl.children[0].innerHTML = data.list[16].dt_txt
-            dayThreeEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[16].weather[0].icon + "@2x.png"
-            dayThreeEl.children[2].innerHTML = "Temperature: " + data.list[16].main.temp
-            dayThreeEl.children[3].innerHTML = "Humidity: " + data.list[16].main.humidity
-            dayThreeEl.children[4].innerHTML = "Wind Speed:" + data.list[16].wind.speed
+      dayThreeEl.children[0].innerHTML = data.list[16].dt_txt
+      dayThreeEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[16].weather[0].icon + "@2x.png"
+      dayThreeEl.children[2].innerHTML = "Temperature: " + data.list[16].main.temp
+      dayThreeEl.children[3].innerHTML = "Humidity: " + data.list[16].main.humidity
+      dayThreeEl.children[4].innerHTML = "Wind Speed:" + data.list[16].wind.speed
 
-            dayFourEl.children[0].innerHTML = data.list[24].dt_txt
-            dayFourEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[24].weather[0].icon + "@2x.png"
-            dayFourEl.children[2].innerHTML = "Temperature: " + data.list[24].main.temp
-            dayFourEl.children[3].innerHTML = "Humidity: " + data.list[24].main.humidity
-            dayFourEl.children[4].innerHTML = "Wind Speed:" + data.list[24].wind.speed
+      dayFourEl.children[0].innerHTML = data.list[24].dt_txt
+      dayFourEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[24].weather[0].icon + "@2x.png"
+      dayFourEl.children[2].innerHTML = "Temperature: " + data.list[24].main.temp
+      dayFourEl.children[3].innerHTML = "Humidity: " + data.list[24].main.humidity
+      dayFourEl.children[4].innerHTML = "Wind Speed:" + data.list[24].wind.speed
 
-            dayFiveEl.children[0].innerHTML = data.list[32].dt_txt
-            dayFiveEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[32].weather[0].icon + "@2x.png"
-            dayFiveEl.children[2].innerHTML = "Temperature: " + data.list[32].main.temp
-            dayFiveEl.children[3].innerHTML = "Humidity: " + data.list[32].main.humidity
-            dayFiveEl.children[4].innerHTML = "Wind Speed:" + data.list[32].wind.speed
-          })
-      }
+      dayFiveEl.children[0].innerHTML = data.list[32].dt_txt
+      dayFiveEl.children[1].src = "https://openweathermap.org/img/wn/" + data.list[32].weather[0].icon + "@2x.png"
+      dayFiveEl.children[2].innerHTML = "Temperature: " + data.list[32].main.temp
+      dayFiveEl.children[3].innerHTML = "Humidity: " + data.list[32].main.humidity
+      dayFiveEl.children[4].innerHTML = "Wind Speed:" + data.list[32].wind.speed
+    })
+}
